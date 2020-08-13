7ae3886461ba09d18ae6671ea0d1592289c30309
DeadPoetSpoon <1149097040@qq.com>
2020-08-13 11:29:38 +0800

    上传远程仓库
    Signed-off-by: DeadPoetSpoon <1149097040@qq.com>

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,66 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <el-container>
+      <el-aside width="155px">
+        <NavMenu @select="update"/>
+      </el-aside>
+      <el-container>
+        <el-main>
+          <keep-alive>
+            <component v-bind:is="current" msg="sssss"></component>
+          </keep-alive>
+          <el-dialog
+              :visible.sync="dialogAdmin">
+            <el-card shadow="hover"><el-link icon="el-s-custom" href="http://pi.deadpoetspoon.xyz:7900/admin/">Django管理面板</el-link></el-card>
+            <span slot="footer" class="dialog-footer">
+              <el-button @click="dialogAdmin = false">取 消</el-button>
+            </span>
+          </el-dialog>
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 </template>
 
 <script>
+import NavMenu from './components/NavMenu.vue'
 import HelloWorld from './components/HelloWorld.vue'
-
+import LinkPanel from './components/LinkPanel.vue'
+import TaskList from './components/TaskList.vue'
 export default {
-  name: 'App',
+  name: 'app',
+  data(){
+    return {
+      current:'LinkPanel',
+      dialogAdmin:false
+    };
+  },
+  methods:{
+    update(com){
+      console.log(com);
+      if(com == "Admin"){
+        this.dialogAdmin = true;
+        return;
+      }
+      this.current = com;
+    }
+  },
   components: {
-    HelloWorld
+    NavMenu,
+    HelloWorld,
+    LinkPanel,
+    TaskList
   }
-}
+} 
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 30px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import './plugins/element.js'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
+Vue.use(VueAxios,axios);
+Vue.prototype.$axios = axios;
 Vue.config.productionTip = false
 
 new Vue({
